baa4124bce857a87879dc79c6d81abb6ce73612c
Ivan <yenirotor@gmail.com>
2021-05-03 09:31:09 +0300

    made some vars of field with help of vebinar added a function user_input

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -6,21 +6,42 @@ import random
 # 3. Кто выйграл
 # 4. Взаимодействие блоков
 
-#var1
+#var1 переменная с отртсовкой поля
 field = [['-','-','-'],
          ['-','-','-'],
          ['-','-','-']]
 print(field)
 #var2
-field = [['-']*3 for _ in range(3)]
-print(field)
+#field = [['-']*3 for _ in range(3)]
+#print(field)
 
-print(*field)
+#print(*field)
 #VAR_3
-print('  0 1 2')
-for i in range(len(field)):
-    print(str(i), *field[i])
+#print('  0 1 2')
+#for i in range(len(field)):
+#    print(str(i), *field[i])
 #var_4
-print('  0 1 2')
-for i in range(len(field)):
-    print(str(i)+' '+' '.join(field[i]))
+#print('  0 1 2')
+#for i in range(len(field)):
+#    print(str(i)+' '+' '.join(field[i]))
+
+#var with 'a b c'
+#num=' a b c'
+#print(num)
+#for row, i in zip(field,num.split()):
+#    print(f"{i} {' '.join(str(j) for j in row)}")
+
+#функция отрисовки поля
+def show_field(f):
+    print('  0 1 2')
+    for i in range(len(field)):
+        print(str(i) + ' ' + ' '.join(field[i]))
+show_field(field)
+
+#опрос пользователя
+def users_input(f):
+    place = input('Введите кооординаты: ').split()
+    if len(place)!=2:
+        print('Ввведите две координаты')
+        continue
+users_input(field)
