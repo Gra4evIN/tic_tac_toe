ae214d8e462e4a69f621a2e506486f6f7d6692b0
Ivan <yenirotor@gmail.com>
2021-05-05 23:09:28 +0300

    Удален весь код. Заново переписан в соответсвии с материалами вебинар

--- tic_tac_toe-master/tic_tac_toe.py
+++ tic_tac_toe-master/tic_tac_toe.py
@@ -1,56 +1,24 @@
-import random
 
-# 1.Поле 3*3
-#
-# 2. ОПрос точки
-# 3. Кто выйграл
-# 4. Взаимодействие блоков
 
-#var1 переменная с отртсовкой поля
-field = [['-','-','-'],
-         ['-','-','-'],
-         ['-','-','-']]
-print(field)
-#var2
-#field = [['-']*3 for _ in range(3)]
-#print(field)
-
-#print(*field)
-#VAR_3
-#print('  0 1 2')
-#for i in range(len(field)):
-#    print(str(i), *field[i])
-#var_4
-#print('  0 1 2')
-#for i in range(len(field)):
-#    print(str(i)+' '+' '.join(field[i]))
-
-#var with 'a b c'
-#num=' a b c'
-#print(num)
-#for row, i in zip(field,num.split()):
-#    print(f"{i} {' '.join(str(j) for j in row)}")
-
-#функция отрисовки поля
 def show_field(f):
-    print('  0 1 2')
-    for i in range(len(field)):
-        print(str(i) + ' ' + ' '.join(field[i]))
-show_field(field)
+    num ='  0 1 2'
+    print(num)
+    #zip
+    for row,i in zip(f,num.split()):
+        print (f"{i} {' '.join(str(j) for j in row)}")
 
-#опрос пользователя
-def users_input(f):
+def users_input(f,user):
     while True:
-        place = input('Введите кооординаты: ').split()
+        place=input(f"Ходит {user} .Введите координаты:").split()
         if len(place)!=2:
             print('Введите две координаты')
             continue
-
+        #is digit str
         if not(place[0].isdigit() and place[1].isdigit()):
             print('Введите числа')
             continue
-        x,y=map(int, place)
-        if not(x >= 0 and x<3 and y>=0 and y<3):
+        x, y = map(int, place)
+        if not(x>=0 and x<3 and y>=0 and  y<3):
             print('Вышли из диапазона')
             continue
         if f[x][y]!='-':
@@ -59,19 +27,44 @@ def users_input(f):
         break
     return x,y
 
-users_input(field)
 
-field=[['-']*3 for _ in range(3)]
-count = 0
-while True:
-    if count==9:
-        print('Ничья')
-        break
-    if count%2==0:
-        user = 'x'
-    else:
-        user = '0'
-    show_field(field)
-    x,y=users_input(field)
-    field[x][y]=user
-    count+=1
+def win_position(f,user):
+    f_list=[]
+    print(f)
+    for l in f:
+        f_list+=l
+    print(f_list)
+    positions=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    indices = set([i for i, x in enumerate(f_list) if x == user])
+
+    for p in positions:
+        if len(indices.intersection(set(p)))==3:
+            return True
+    return False
+
+
+def start(field):
+
+    count=0
+    while True:
+        show_field(field)
+        if count%2==0:
+            user='x'
+        else:
+            user = 'o'
+        if count<9:
+            x, y = users_input(field,user)
+            field[x][y] = user
+
+        elif count==9:
+            print ('Ничья')
+            break
+        if win_position(field,user):
+            print(f"Выйграл {user}")
+            break
+        count+=1
+
+
+field = [['-'] * 3 for _ in range(3)]
+
+start(field)
